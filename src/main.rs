20fa9dbec495826eff2a8359023cfcffbf4f6a19
S Smith <root@madeof.glass>
2022-10-20 16:46:04 -0500

    Refactor for elegance
    This refactor removes the list of fieldnotes specified in the code,
    replacing it with a json file in the root. Additionally, it uses
    compiled-in-binary sources, rather than reading from the filesystem,
    allowing for universal portability.
    
    Signed-off-by: S Smith <root@madeof.glass>

--- src/main.rs
+++ src/main.rs
@@ -1,148 +1,140 @@
+use include_dir::{include_dir, Dir};
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use tera::{Tera,Context};
+use warp::Filter;
+use comrak::{markdown_to_html, ComrakOptions};
 use chrono::prelude::{DateTime, NaiveDateTime};
 use chrono::Utc;
-use comrak::{markdown_to_html, ComrakOptions};
-use lazy_static::lazy_static;
-use serde::{Deserialize, Serialize};
-use tera::{Context, Tera};
-use warp::Filter;
 
-#[tokio::main]
-async fn main() {
-    let (css, js, staticfiles, favicon) = (
-        warp::path("js").and(warp::fs::dir("js")),
-        warp::path("static").and(warp::fs::dir("static")),
-        warp::path("favicon.ico").and(warp::fs::file("favicon.ico")),
-        warp::path("css").and(warp::fs::dir("css")),
-    );
+const CONTENT_DIR: Dir = include_dir!("src/content");
+const METADATA: &str = include_str!("fieldnotes.json");
+const STATIC_DIR: Dir = include_dir!("src/static");
 
-    let fieldnotes = get_fieldnotes();
-    let mut context = get_default_context().unwrap();
-    context.insert("entries", &fieldnotes);
-    context.insert("title", "Field Notes");
-
-    let index = warp::path::end().map(move || {
-        warp::reply::html(TEMPLATES.render("index.html", &context).unwrap())
-    });
-
-    let notes = warp::path::param()
-        .map(move |path: String| {
-            let note_context = get_default_context();
-            
-            match fieldnotes.get(&path) {
-                Some(note) => note.render(note_context),
-                None => FieldNote {
-                    date_epoch: 0,
-                    title: "End of the Road".to_string(),
-                    slug: "end-of-the-road".to_string(),
-                    body: String::new(),
-                }.render(note_context)
-            }
-        });
-
-    let routes = warp::get().and(
-        staticfiles
-        .or(favicon)
-        .or(css)
-        .or(js)
-        .or(index)
-        .or(notes)
-    );
-
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
-}
-
-fn get_default_context() -> Option<Context> {
-    Some(Context::new())
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Clone)]
 pub struct FieldNote {
     date_epoch: i64,
     title: String,
-    slug: String,
     body: String,
+    slug: String
 }
 
 impl FieldNote {
-    pub fn new(
-        date_epoch: i64,
-        title: String,
-        slug: String,
-    ) -> Self {
-        Self {
-            date_epoch,
-            title: title.to_string(),
-            slug: slug.to_string(),
-            body: match Tera::new(r#"src/notes/**/*"#) {
-                Ok(t) => match t.render(&slug, &Context::new()) {
-                    Ok(rend) => markdown_to_html(&rend, &ComrakOptions::default()),
-                    Err(e) => {
-                        println!("Rendering error(s): {}", e);
-                        String::from("Rendering error(s)")
-                    }
-                },
-                Err(e) => {
-                    println!("Rendering error(s): {}", e);
-                    String::from("Rendering error(s)")
-                }
-            },
-        }
-    }
     pub fn render(&self, maybe_context: Option<Context>) -> impl warp::Reply {
+        let tera = preload_templates();
         let mut ctx = match Context::from_serialize(&self) {
             Ok(c) => match maybe_context {
                 Some(mut cx) => {
                     cx.extend(c);
                     cx
                 },
-                None => c,
+                None => c
             },
-            Err(e) => panic!("Serialization error(s): {}",e)
+            Err(e) => panic!("Serialization error(s): {}", e),
         };
 
         let naive = NaiveDateTime::from_timestamp(self.date_epoch,0);
         let datetime: DateTime<Utc> = DateTime::from_utc(naive, Utc);
         ctx.insert("date", &datetime.format("%B %-d, %Y").to_string());
 
-        let base  = TEMPLATES.render("note.html", &ctx);
-        warp::reply::html(match base {
-            Ok(b) => b,
-            Err(b) => {
-                println!("Rendering error(s): {}", b);
-                String::new()
-            }
-        })
+        match tera.render("note.html", &ctx) {
+            Ok(rend) => warp::reply::html(rend),
+            Err(e) => warp::reply::html(e.to_string()),
+        }
     }
 }
 
-// templating
-lazy_static! {
-    pub static ref TEMPLATES: Tera = {
-        match Tera::new("templates/**/*.html") {
-            Ok(t) => t,
-            Err(e) => {
-                println!("Parsing error(s): {}", e);
-                ::std::process::exit(1);
-            }
-        }
-    };
+#[derive(Deserialize)]
+pub struct FieldMetadata {
+    title: String,
+    date: i64,
+    slug: String,
 }
 
-/// Metadata entry and data reconstruction
-fn get_fieldnotes() -> HashMap<String,FieldNote> {
+#[tokio::main]
+async fn main() {
+    STATIC_DIR.extract("/tmp/fieldnotes").expect("Couldn't extract field notes.");
     
-    let notes = vec![
-        // slug, title, date epoch
-        ("silence", "My repository sits in silence.",1666217144),
-        ("test-syntax", "syntax highlighting example ", 1646217544),
-    ];
+    let staticfiles = warp::path("static").and(warp::fs::dir("/tmp/fieldnotes"));
+    let favicon = warp::path("favicon.ico").and(warp::fs::dir("/tmp/fieldnotes/favicon.ico"));
     
-    let mut notes_hash: HashMap<String,FieldNote> = HashMap::<String,FieldNote>::new();
+    let tera = preload_templates();
+    let fieldnotes = preload_notes();
+    let mut context = get_default_context().unwrap();
+    context.insert("entries", &fieldnotes);
+    context.insert("title", "Field Notes");
+    
+    let index = warp::path::end().map(move || warp::reply::html(tera.render("index.html", &context).unwrap()));
+    
+    let notes = warp::path::param()
+        .map(move |path:String| {
+            let note_context = get_default_context();
+            match fieldnotes.get(&path) {
+                Some(note) => note.render(note_context),
+                None => FieldNote {
+                    date_epoch: 0,
+                    title: "End of the Road".to_string(),
+                    slug: "end-of-the-road".to_string(),
+                    body: String::new()
+                }.render(note_context)
+            }
+        });
+    
+    let routes = warp::get().and(
+        staticfiles
+        .or(favicon)
+        .or(index)
+        .or(notes)
+    );
 
-    notes.iter().for_each(|f| {
-        notes_hash.insert(f.0.to_string(), FieldNote::new(f.2, f.1.to_string(),f.0.to_string()));
-    });
+    warp::serve(routes).run(([127,0,0,1],3030)).await;
+}
+
+fn get_default_context() -> Option<Context> {
+    Some(Context::new())
+}
+
+fn preload_templates() -> Tera {
+    let mut tera = Tera::default();
+
+    tera.add_raw_templates(vec![
+        ("base.html", include_str!("templates/base.html")),
+        ("index.html", include_str!("templates/index.html")),
+        ("footer.html", include_str!("templates/footer.html")),
+        ("nav.html", include_str!("templates/nav.html")),
+        ("note.html", include_str!("templates/note.html")),
+    ])
+    .expect("Error: couldn't preload templates");
+
+    tera
+}
+
+fn preload_notes() -> HashMap<String, FieldNote> {
+    let mut notes_hash = HashMap::<String, FieldNote>::new();
+    
+    let notes_metadata: Vec<FieldMetadata> =
+        serde_json::from_str(METADATA).expect("JSON not well formatted");
+    
+    for entry in notes_metadata {
+        let preloaded = CONTENT_DIR.get_file(entry.slug.clone());
+        let entry_body = match preloaded {
+            Some(f) => match f.contents_utf8() {
+                Some(b) => markdown_to_html(b, &ComrakOptions::default()),
+                None => String::new(),
+            },
+            None => String::new(),
+        };
+        
+        notes_hash.insert(
+            entry.slug.clone(),
+            FieldNote {
+                date_epoch: entry.date,
+                title: entry.title,
+                body: entry_body,
+                slug: entry.slug.clone()
+            },
+        );
+    }
 
     notes_hash
 }
